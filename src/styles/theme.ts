ef18206fd898fd4bf6f3339a95cb1bcf81449b78
Bartosz Maksimowski <bartosz.maksimowski@develoart.com>
2023-11-06 14:33:34 +0100

    Theme Added

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,1 +1,9 @@
-export const theme = {}
+export const theme = {
+  colors: {
+    dark: '#120907',
+    lightDark: '#191e36',
+    medium: '#485a8c',
+    lightBlue: '#849be5',
+    bright: '#bfd7ed',
+  },
+}
