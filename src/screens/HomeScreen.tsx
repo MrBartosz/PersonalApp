a76c146bada8d952ee6b6e77fc8b08d62a86c526
Bartosz Maksimowski <bartosz.maksimowski@develoart.com>
2023-11-08 14:05:34 +0100

    add menus

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,1 +1,11 @@
-export const HomeScreen = () => {}
+import { StyleSheet, Text, View } from 'react-native'
+
+export const HomeScreen = () => {
+  return (
+    <View>
+      <Text>Witam eaweasdsdsadsadasdasdadsads1asa</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
